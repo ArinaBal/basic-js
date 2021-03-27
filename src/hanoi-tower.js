1472389d978b99a88c64ac5e7d5fd761fe0d2d89
ArinaBal <ariane.93@mail.ru>
2021-03-28 01:31:38 +0300

    feat: add solution for hanoi-tower task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  //Рекурсивно решаем задачу «перенести башню из n−1 диска на 2-й штырь». Затем переносим самый большой диск на 3-й штырь, и рекурсивно решаем задачу «перенеси башню из n−1 диска на 3-й штырь».
+  //Отсюда методом математической индукции заключаем, что минимальное число ходов, необходимое для решения головоломки, равно 2n − 1, где n — число дисков)
+  // Найдем число ходов, необходимое для решения головоломки
+  let turns = Math.pow(2, disksNumber) - 1;
+  //Найдем число секунд, необходимое для решения головоломки
+  let seconds = Math.floor((turns * 3600) / turnsSpeed);
+  return {turns, seconds}
 };

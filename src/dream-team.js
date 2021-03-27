18a1c32ba116388c94234940895695a04c881caf
ArinaBal <ariane.93@mail.ru>
2021-03-28 00:06:28 +0300

    feat: add solution for task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  //Зададим пустой массив для названия нашей команды
+  let name = [];
+  //Проверяем тип аргумента функции (Метод Array.isArray() возвращает true, если объект является массивом и false, если он массивом не является)
+  if (!Array.isArray(members)) {
+    return false
+  };
+  //для каждого элемента массива проверим являются ли они типом string
+  members.forEach((member) => {
+    if (typeof member == "string") {
+      //Создаем массив названия из первых букв имени без пробелов и с заглавных букв
+      name.push(member.trim().charAt(0).toUpperCase());
+    }
+    return name;
+  })
+  //Сортируем по алфавиту, объединяем  в одну строку
+  return name.sort().join('');
 };

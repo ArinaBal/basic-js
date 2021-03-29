a4374287093323f9e1945e76984487c2bee52467
ArinaBal <ariane.93@mail.ru>
2021-03-29 21:55:09 +0300

    feat: add solution fot extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,50 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+//str это строка, которая будет повторена
+//options это объект опций, который содержит следующие свойства:
+//repeatTimes устанавливает число повторений str
+//separator это строка, разделяющая повторения str
+//addition это дополнительная строка, которая будет добавлена после каждого повторения str
+//additionRepeatTimes устанавливает число повторений addition
+//additionSeparator это строка, разделяющая повторения addition
+module.exports = function repeater(str, options) {
+  //зададим значения по умолчанию для параметров
+  const {
+    repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1,
+    additionSeparator = '|'
+  } = options;
+
+
+
+  //Если число повторений строки не определено, возвращаем строку
+  if (repeatTimes === undefined) {
+    return ''
+  }
+
+  //Зададим массив с повторяющейся строкой
+  let  repeatStr = [];
+
+  for (let i = 0; i < repeatTimes; i++) {
+  //Добавляем в массив повторяющиеся строки
+    repeatStr.push(str);
+  }
+// Зададим массив с повторяющейся доп. строкой
+  let repeatAdd = [];
+
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    //Добавляем в массив повторяющиеся доп. строки
+    repeatAdd.push(`${addition}`);
+  }
+
+  //Разделим массив доп. строк
+ let repeatAddSep = repeatAdd.join(additionSeparator);
+
+//К каждому элементу массива повторяющихся строк добавим массив разделенных повторяющихся доп. строк
+  let repeatStrAdd = repeatStr.map((element) => element + repeatAddSep)
+  //Разделим получившийся массив
+  let repeatStrAddSep = repeatStrAdd.join(separator);
+  return repeatStrAddSep;
+}

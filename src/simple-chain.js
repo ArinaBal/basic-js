ca3613bb4ef68e7eb3fbbaf6a6f544471e200093
ArinaBal <ariane.93@mail.ru>
2021-03-29 21:19:16 +0300

    feat: add solution for simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,50 @@
 const CustomError = require("../extensions/custom-error");
+//getLength возвращает текущую длину цепи в виде числа;
+//addLink(value) добавляет звено, содержащее строковое представление value к цепочке;
+//removeLink(position) удаляет звено цепи, находящееся в заданном положении;
+//reverseChain разворачивает цепь задом наперед;
+//finishChain завершает цепь и возвращает ее.
+
+//Введем переменную массив цепочки
+let chain = [];
 
 const chainMaker = {
+  //Длина цепи равняется длине массива chain
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+   return chain.lenght;
   },
+  //Добавляем звено
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
+    //Если addLink вызван без аргументов, он добавляет пустое звено ('( )') в цепочку
+   if(value === undefined) {
+       chain.push(' ');
+       return chainMaker;
+     }
+    //Добавляем строковое представление value к цепочке
+    chain.push(`( ${value} )`);
+     return chainMaker;
+   },
+  //Удаляем звено в цепи
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
+    //Если положение является положительным целым числом 
+    if (typeof position === "number") {
+      chain.splice(position - 1, 1);
+      return chainMaker;
+    } else {
+      chain = [];
+      throw Error();
+    }
+  }, 
+  //Разворачиваем цепь
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
+   chain.reverse();
+     return chainMaker;
+   },
+  //Завершаем цепь
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = chain.join('~~');
+    chain.splice(0, chain.length)
+    return result;
   }
 };
-
 module.exports = chainMaker;
